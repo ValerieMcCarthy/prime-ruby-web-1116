00414e8022ad0608c2ff1da50c169d2a14bbff7e
Valerie McCarthy <valeriewilsonmccarthy@gmail.com>
2016-11-05 16:26:22 -0400

    Prime numbers found.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime? (num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
+end
+
+
+
+
+
+# Checking everything until the square root
+# (and not going over) ensures you check all possible factors.
+# For example, checking if 100 is prime involves checking
+# everything up to 10. Checking 99 means only going to 9.
